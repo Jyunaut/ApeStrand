e35c867bf3e61df00096fc811b89cea7c1ad4f1d
Jyunaut <edmartinh@gmail.com>
2021-05-15 03:52:42 -0700

    updated adjacent platform logic

---
+++ Assets/Scripts/Materials/Raft.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raft : MonoBehaviour
+{
+    public List<GameObject> Platforms = new List<GameObject>();
+}
