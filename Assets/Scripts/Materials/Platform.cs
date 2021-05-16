02c8af2252d6296f830b8c8614ae910b722a1fbc
Jyunaut <edmartinh@gmail.com>
2021-05-16 00:30:48 -0700

    Changed "AdjacentPlatforms" to "UpdateColliders"

--- Assets/Scripts/Materials/Platform.cs
+++ Assets/Scripts/Materials/Platform.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -6,17 +7,23 @@ public class Platform : MonoBehaviour, IDamageable
     [SerializeField]
     private EdgeCollider2D[] _walls = new EdgeCollider2D[4];
 
+    public List<GameObject> _adjacentPlatforms = new List<GameObject>();
+
+    // TODO: Send a message to all adjacent platforms to update their colliders
     public void TakeDamage()
     {
         print(gameObject.name + " takes damage");
+        UpdateColliders();
     }
 
     /*--------------------------------------------------------------------------------------------- 
     * Cast a ray in all four directions (right, up, left, down) and check if there is a platform
-    * adjacent to this one.
+    * adjacent to this one.  If there is an adjacent platform, disable the corresponding edge collider.
     */
-    void CheckAdjacent()
+    void UpdateColliders()
     {
+        _adjacentPlatforms = new List<GameObject>();
+
         float yOffset  = 1.755f - 0.9225f; // Calculated from the edge collider height
         Vector2 origin = (Vector2)transform.position + new Vector2(0, yOffset);
         float rotation = 90;
@@ -31,13 +38,23 @@ public class Platform : MonoBehaviour, IDamageable
                                            .Where(hit => hit.collider.gameObject != gameObject).ToArray();
             // Debug.DrawLine(origin, origin + new Vector2(xLen * direction.x, yLen * direction.y), Color.red);
             
-            if (hits.Length == 0)     _walls[i].enabled = true;
-            foreach (var hit in hits) _walls[i].enabled = false;
+            if (hits.Length == 0)
+            {
+                _walls[i].enabled = true;
+                continue;
+            }
+
+            foreach (var hit in hits)
+            {
+                _walls[i].enabled = false;
+                if (!_adjacentPlatforms.Contains(hit.collider.gameObject))
+                    _adjacentPlatforms.Add(hit.collider.gameObject);
+            }
         }
     }
 
-    void Update()
+    void OnMouseDown()
     {
-        CheckAdjacent();
+        TakeDamage();
     }
 }
