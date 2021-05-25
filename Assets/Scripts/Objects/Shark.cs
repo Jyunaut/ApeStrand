bbddba5331b33c1232e3e3131ff5a97cbb12566f
CRAKAAW <crowshiding@gmail.com>
2021-05-25 14:57:59 -0700

    Adding WIP shark scripts

---
+++ Assets/Scripts/Objects/Shark.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shark : MonoBehaviour
+{
+    public int health = 2;
+    public GameObject target;
+    
+    public void Attack() {
+
+    }
+
+    public void Swim() {
+
+    }
+
+    public void Patrol() {
+
+    }
+
+    public void FindTarget() {
+
+    }
+}
